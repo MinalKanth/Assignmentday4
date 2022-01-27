4e3318a99e20ff364d042afc87702a57f74aef95
MinalKanth <98406879+MinalKanth@users.noreply.github.com>
2022-01-27 21:30:38 +0530

    Add files via upload

---
+++ scriptday4.js
@@ -0,0 +1,11 @@
+function printTable() {
+
+for ( let i = 1; i <= 10; i++)
+{
+    console.log(num + '*' + i + '=' + num * i);
+}
+
+}
+ const num = prompt('Enter the number');
+
+ printTable (num);
